a2c45e760eb59bf38af53327125c2a330a30bc7e
tvnte <tobi-vente@web.de>
2023-02-18 08:46:20 +0100

    bugfix

--- tests/utils/test_validation_split.py
+++ tests/utils/test_validation_split.py
@@ -12,10 +12,13 @@ class TestValidationSplit(unittest.TestCase):
         self.df = pd.DataFrame(np.ones((100, 3)), columns=["user", "item", "rating", ])
 
     def test_validationSplit_givenValidDataFrame_correctSplitTrainAndValidationDataframesReturnedExpected(self):
-        train, validation = validation_split(self.df, 0.25, 42)
+        val_fold_indices = validation_split(data=self.df, frac=0.25, random_state=42)
 
-        self.assertTrue(train.shape == (75, 3))
-        self.assertTrue(validation.shape == (25, 3))
+        validation_train = self.df.loc[val_fold_indices[0]["train"], :]
+        validation_test = self.df.loc[val_fold_indices[0]["validation"], :]
+
+        self.assertTrue(validation_train.shape == (75, 3))
+        self.assertTrue(validation_test.shape == (25, 3))
 
 
 if __name__ == '__main__':
