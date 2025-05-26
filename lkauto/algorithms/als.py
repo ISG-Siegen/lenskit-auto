5a956ddd7cdd26a1ad8161fe1469fbcead514ddd
EinfachAnass <anassamzid@gmail.com>
2025-05-26 20:53:55 +0200

    updated ALS

--- lkauto/algorithms/als.py
+++ lkauto/algorithms/als.py
@@ -1,8 +1,8 @@
-from lenskit.algorithms import als
-from ConfigSpace import Integer, Float, ConfigurationSpace, Categorical
+from lenskit.als import BiasedMFScorer, ImplicitMFScorer
+from ConfigSpace import ConfigurationSpace, UniformIntegerHyperparameter, UniformFloatHyperparameter, CategoricalHyperparameter
 
 
-class ImplicitMF(als.ImplicitMF):
+class ImplicitMF(ImplicitMFScorer):
     def __init__(self, features, **kwargs):
         super().__init__(features=features, **kwargs)
 
@@ -11,23 +11,16 @@ class ImplicitMF(als.ImplicitMF):
         """
               return default configurationspace
         """
-
-        """
-        The Agorithm is based on the https://ieeexplore.ieee.org/document/4781121 papaer.
-        The paper does not give clear hints on how to set up the hyperparameters best.
-        Therefore we stick to the ranges from LensKits other Matrix Factorization algorithm: BiasedMF
-        """
-        features = Integer('features', bounds=(5, 10000), default=1000)  # No default values given
-        ureg = Float('ureg', bounds=(0.01, 0.1), default=0.1)
-        ireg = Float('ireg', bounds=(0.01, 0.1), default=0.1)
+        features = UniformIntegerHyperparameter('features', lower=5, upper=10000, log=True)
+        ureg = UniformFloatHyperparameter('ureg', lower=0.01, upper=0.1, default_value=0.1, log=True)
+        ireg = UniformFloatHyperparameter('ireg', lower=0.01, upper=0.1, default_value=0.1, log=True)
 
         cs = ConfigurationSpace()
-        cs.add_hyperparameters([features, ureg, ireg])
-
+        cs.add([features, ureg, ireg])
         return cs
 
 
-class BiasedMF(als.BiasedMF):
+class BiasedMF(BiasedMFScorer):
     def __init__(self, features, **kwargs):
         super().__init__(features=features, **kwargs)
 
@@ -46,8 +39,9 @@ class BiasedMF(als.BiasedMF):
         Since the authors just evaluated on one larger dataset, we still allow smaller and larger feature numbers
         but set the default value to 1000.
         """
-        features = Integer('features', bounds=(2, 10000), default=1000, log=True)  # No default value given
-
+        #features = Integer('features', bounds=(2, 10000), default=1000, log=True)  # No default value given
+        # no default value given but we set the default value to 1000???
+        features = UniformIntegerHyperparameter('features', lower=5, upper=10000, log=True)
         """
         The authors of the original ALS paper set the range of the regularization hyperparameter to from 0.03 - 0.065.
         https://link.springer.com/chapter/10.1007/978-3-540-68880-8_32
@@ -55,16 +49,16 @@ class BiasedMF(als.BiasedMF):
         LensKit sets the default regularization hyperparameter to 0.1 Therefore we set the upper bound of the two
         regularization parameters (ureg and ireg) to 0.1.
         """
-        ureg = Float('ureg', bounds=(0.01, 0.1), default=0.1)
-        ireg = Float('ireg', bounds=(0.01, 0.1), default=0.1)
+        ureg = UniformFloatHyperparameter('ureg', lower=0.01, upper=0.1, default_value=0.1, log=True)
+        ireg = UniformFloatHyperparameter('ireg', lower=0.01, upper=0.1, default_value=0.1, log=True)
+
 
         """
         The damping hyperparameter en- or disables a damping factor.
         In the future we may want to tune the damping values as well.
         """
-        bias = Categorical('bias', items=[True, False], default=True)
+        bias = CategoricalHyperparameter('bias', choices=[True, False], default_value=True)
 
         cs = ConfigurationSpace()
-        cs.add_hyperparameters([features, ureg, ireg, bias])
-
+        cs.add([features, ureg, ireg, bias])
         return cs
