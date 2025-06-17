b1829a2b7f791cfb319a95179e301b0e9ee62d96
EinfachAnass <anassamzid@gmail.com>
2025-06-17 20:34:44 +0200

    testing for user knn is OK

--- tests/algorithms/test_user_knn.py
+++ tests/algorithms/test_user_knn.py
@@ -7,17 +7,21 @@ from lkauto.algorithms.user_knn import UserUser
 
 class TestUserKNN(unittest.TestCase):
     def test_init_givenObjectInitialized_ObjectInitializedCorrectlyExpected(self):
-        nnbrs = 10
-        user_user = UserUser(nnbrs)
-        self.assertEqual(nnbrs, user_user.nnbrs)
+        max_n = 100
+        min_n = 5
+        min_s = 0.001
+        user_user = UserUser(max_n, min_nbrs=min_n, min_sim=min_s)
         self.assertIsInstance(user_user, UserUser)
+        self.assertEqual(max_n, user_user.max_nbrs)
+        self.assertEqual(min_n, user_user.min_nbrs)
+        self.assertEqual(min_s, user_user.min_sim)
 
     def test_defaultConfigspace_GivenFunctionCalled_correctConfigSpaceReturnedExpected(self):
         cs = UserUser.get_default_configspace()
-        params = cs.get_hyperparameters()
+        params = list(cs.values())
         self.assertIsInstance(cs, CS.ConfigurationSpace)
         self.assertTrue(any(
-            param.name == "nnbrs" and param.default_value == 1000 and param.lower == 1 and param.upper == 10000 for
+            param.name == "max_nbrs" and param.default_value == 1000 and param.lower == 1 and param.upper == 10000 for
             param in params))
         self.assertTrue(any(
             param.name == "min_nbrs" and param.default_value == 1 and param.lower == 1 and param.upper == 1000 for
