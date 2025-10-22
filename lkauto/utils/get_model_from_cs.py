07057b9ea8a8cbaf0bcae4135647eacfd9d6db4f
catnatsuki <rgkulkarni2002@gmail.com>
2025-10-22 07:06:04 +0200

    Changed get_model_from_cs.py to build configs for each of the algos based on the new API calls for lenskit 2025. Fixed the error in the test_get_model_from_cs.py file where an invalid parameter was being passed to the Bias Algorithm.

--- lkauto/utils/get_model_from_cs.py
+++ lkauto/utils/get_model_from_cs.py
@@ -2,13 +2,14 @@ from typing import Union
 
 from ConfigSpace import ConfigurationSpace
 
-from lenskit.als import BiasedMFScorer
-from lenskit.als import ImplicitMFScorer
+from lenskit.als import BiasedMFScorer, BiasedMFConfig
+from lenskit.data.types import UIPair
+from lenskit.als import ImplicitMFScorer, ImplicitMFConfig
 from lenskit.basic import BiasScorer
-from lenskit.funksvd import FunkSVDScorer
-from lenskit.knn import ItemKNNScorer
-from lenskit.sklearn.svd import BiasedSVDScorer
-from lenskit.knn import UserKNNScorer
+from lenskit.funksvd import FunkSVDScorer, FunkSVDConfig
+from lenskit.knn import ItemKNNScorer, ItemKNNConfig
+from lenskit.sklearn.svd import BiasedSVDScorer, BiasedSVDConfig
+from lenskit.knn import UserKNNScorer, UserKNNConfig
 # from lenskit.scored import Scorer
 
 
@@ -55,43 +56,77 @@ def get_model_from_cs(cs: ConfigurationSpace,
 
     # ItemItem
     if algo_name == 'ItemItem':
-        model = ItemKNNScorer(feedback=feedback, **config)
-        # model = ItemKNNScorer(feedback=feedback)
+        # model = ItemKNNScorer(feedback=feedback, **config)
+        cfg = ItemKNNConfig(feedback=feedback,
+                            max_nbrs=config.get('max_nbrs',20),
+                            min_nbrs=config.get('min_nbrs',1),
+                            min_sim=config.get('min_sim', 1e-6))
+        model = ItemKNNScorer(config=cfg)
     # UserUser
     elif algo_name == 'UserUser':
-        model = UserKNNScorer(feedback=feedback, **config)
-        # model = UserKNNScorer(feedback=feedback)
+        # model = UserKNNScorer(feedback=feedback, **config)
+        cfg = UserKNNConfig(feedback=feedback,
+                            max_nbrs=config.get('max_nbrs', 20),
+                            min_nbrs=config.get('min_nbrs', 1),
+                            min_sim=config.get('min_sim', 1e-6))
+        model = UserKNNScorer(config=cfg)
     # FunkSVD
     elif algo_name == 'FunkSVD':
-        model = FunkSVDScorer(random_state=random_state, **config)
-        # model = FunkSVDScorer(feedback=feedback)
+        # model = FunkSVDScorer(random_state=random_state, **config)
+        cfg = FunkSVDConfig(
+            embedding_size=int(config.get('features', 50)),  # default 50
+            learning_rate=float(config.get('lrate', 0.001)),  # default 0.001
+            regularization=float(config.get('reg', 0.015)),  # default 0.015
+            damping=float(config.get('damping', 5.0)),  # default 5.0
+        )
+        model = FunkSVDScorer(config=cfg)
     # BiasedSVD
     elif algo_name == 'BiasedSVD':
-        model = BiasedSVDScorer(**config)
-        # model = BiasedSVDScorer(feedback=feedback)
+        # model = BiasedSVDScorer(**config)
+        damping_dict = {
+            'user': float(config.get('user_damping', 5.0)),  # default 5
+            'item': float(config.get('item_damping', 5.0))  # default 5
+        }
+        cfg = BiasedSVDConfig(
+            damping=damping_dict
+        )
+        model = BiasedSVDScorer(config=cfg)
     # ALSBiasedMF
     elif algo_name == 'ALSBiasedMF':
-        reg_touple = (float(config.pop('ureg')), float(config.pop('ireg')))
+        # reg_touple = (float(config.pop('ureg')), float(config.pop('ireg')))
         print(config.keys()) #delete later for debugging
         # del config['ureg']
         # del config['ireg']
-        model = BiasedMFScorer(reg=reg_touple, rng_spec=random_state, **config)
-        # model = BiasedMFScorer(feedback=feedback)
+        # model = BiasedMFScorer(reg=reg_touple, rng_spec=random_state, **config)
+        reg = UIPair(
+            user=config.get('ureg', 0.1),
+            item=config.get('ireg', 0.1)
+        )
+        cfg = BiasedMFConfig(
+            embedding_size=int(config.get('features', 50)),
+            regularization=reg,
+            user_embeddings=True if config.get('bias', True) else 'prefer',
+        )
+        model = BiasedMFScorer(config=cfg)
     # Biased
     elif algo_name == 'Bias':
-        user_damping = float(config.pop('user_damping'))
-        item_damping = float(config.pop('item_damping'))
+        user_damping = float(config.get('user_damping'))
+        item_damping = float(config.get('item_damping'))
 
         # New API: pass as dict instead of tuple
         damping_dict = {'user': user_damping, 'item': item_damping}
-        model = BiasScorer(damping=damping_dict, **config)
+        model = BiasScorer(damping=damping_dict)
+        # model = BiasScorer(damping=damping_dict, **config)
     # ImplicitMF
     elif algo_name == 'ImplicitMF':
-        reg_touple = (float(config.pop('ureg')), float(config.pop('ireg')))
-        # del config['ureg']
-        # del config['ireg']
-        model = ImplicitMFScorer(reg=reg_touple, rng_spec=random_state, **config)
-        # model = ImplicitMFScorer(feedback=feedback)
+        # reg_touple = (config.pop('ureg'), config.pop('ireg'))
+        # model = ImplicitMFScorer(reg=reg_touple, rng_spec=random_state, **config)
+        cfg = ImplicitMFConfig(
+            embedding_size=int(config.get('features', 50)),
+            # regularization=reg_touple,
+            user_embeddings=True if config.get('bias', True) else 'prefer',
+        )
+        model = ImplicitMFScorer(config=cfg)
     else:
         raise ValueError("Unknown algorithm: {}".format(algo_name))
 
